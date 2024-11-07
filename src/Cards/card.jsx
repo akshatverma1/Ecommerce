34d372e0903328608e6481ca15f0b77cc0fd319e
akshatverma1 <akshaverma14@gmail.com>
2024-11-07 16:40:37 +0530

    Json Data

--- src/Cards/card.jsx
+++ src/Cards/card.jsx
@@ -7,9 +7,9 @@ export default function CardFunction({id,productName,description,productPrices})
             <div class="card">
                 <div class="card-details">
                     <img src={Image} alt="Sample Image" width="50px" height="50px"/>
-                    <p class="text-title">{id}</p>
+                    <p>{id}</p>
                     <p>{productName}</p>
-                    <p>{description}</p>
+                    {/* <p>{description}</p> */}
                     <p>{productPrices}</p>
                 </div>
                 <button class="card-button">Add To Cart</button>
--- src/homepage.jsx
+++ src/homepage.jsx
@@ -2,80 +2,32 @@ import React from 'react';
 import "./homepage.css"
 import Card from "./Cards/card.jsx";
 import Navbar from './Navbar/navbar.jsx';
+import JsonData from "./ItemData/electronics_products.json";
+
 export default function Homepagefunction() {
-    let data = [{
-        id: 1,
-        productName: "Iphone 16",
-        prices: "₹70000",
-        description: "The iPhone is a series of smartphones made by Apple Inc. since 2007. It is a mobile phone, meaning that it makes calls and sends text messages without wires. There are many types of iPhones. The iPhone can access the Internet as well, either using a cellular network or over Wi-Fi",
-    },
-    {
-        id: 2,
-        productName: "Iphone 16",
-        prices: "₹70000",
-        description: "The iPhone is a series of smartphones made by Apple Inc. since 2007. It is a mobile phone, meaning that it makes calls and sends text messages without wires. There are many types of iPhones. The iPhone can access the Internet as well, either using a cellular network or over Wi-Fi",
-    },
-    {
-        id: 3,
-        productName: "Iphone 16",
-        prices: "₹70000",
-        description: "The iPhone is a series of smartphones made by Apple Inc. since 2007. It is a mobile phone, meaning that it makes calls and sends text messages without wires. There are many types of iPhones. The iPhone can access the Internet as well, either using a cellular network or over Wi-Fi",
-    },
-    {
-        id: 4,
-        productName: "Iphone 16",
-        prices: "₹70000",
-        description: "The iPhone is a series of smartphones made by Apple Inc. since 2007. It is a mobile phone, meaning that it makes calls and sends text messages without wires. There are many types of iPhones. The iPhone can access the Internet as well, either using a cellular network or over Wi-Fi",
-    },
-    {
-        id: 5,
-        productName: "Iphone 16",
-        prices: "₹70000",
-        description: "The iPhone is a series of smartphones made by Apple Inc. since 2007. It is a mobile phone, meaning that it makes calls and sends text messages without wires. There are many types of iPhones. The iPhone can access the Internet as well, either using a cellular network or over Wi-Fi",
-    },
-    {
-        id: 6,
-        productName: "Iphone 16",
-        prices: "₹70000",
-        description: "The iPhone is a series of smartphones made by Apple Inc. since 2007. It is a mobile phone, meaning that it makes calls and sends text messages without wires. There are many types of iPhones. The iPhone can access the Internet as well, either using a cellular network or over Wi-Fi",
-    },
-    {
-        id: 7,
-        productName: "Iphone 16",
-        prices: "₹70000",
-        description: "The iPhone is a series of smartphones made by Apple Inc. since 2007. It is a mobile phone, meaning that it makes calls and sends text messages without wires. There are many types of iPhones. The iPhone can access the Internet as well, either using a cellular network or over Wi-Fi",
-    }, {
-        id: 8,
-        productName: "Iphone 16",
-        prices: "₹70000",
-        description: "The iPhone is a series of smartphones made by Apple Inc. since 2007. It is a mobile phone, meaning that it makes calls and sends text messages without wires. There are many types of iPhones. The iPhone can access the Internet as well, either using a cellular network or over Wi-Fi",
-    }, {
-        id: 9,
-        productName: "Iphone 16",
-        prices: "₹70000",
-        description: "The iPhone is a series of smartphones made by Apple Inc. since 2007. It is a mobile phone, meaning that it makes calls and sends text messages without wires. There are many types of iPhones. The iPhone can access the Internet as well, either using a cellular network or over Wi-Fi",
-    }, {
-        id: 10,
-        productName: "Iphone 16",
-        prices: "₹70000",
-        description: "The iPhone is a series of smartphones made by Apple Inc. since 2007. It is a mobile phone, meaning that it makes calls and sends text messages without wires. There are many types of iPhones. The iPhone can access the Internet as well, either using a cellular network or over Wi-Fi",
-    }, {
-        id: 11,
-        productName: "Iphone 16",
-        prices: "₹70000",
-        description: "The iPhone is a series of smartphones made by Apple Inc. since 2007. It is a mobile phone, meaning that it makes calls and sends text messages without wires. There are many types of iPhones. The iPhone can access the Internet as well, either using a cellular network or over Wi-Fi",
-    }];
-    return (<>
-       
-        <div className="maine">
-            <center>
-                <div className='cardse'>
-                    {data.map((item, index) => (
-                        <Card id={item.id} productName={item.productName} description={item.description} productPrices = {item.prices}></Card>
-                    ))}
-                </div>
-            </center>
-        </div>
-    </>
+    
+
+    async function call(){
+        try {
+            await fetch("./ItemData/electronics_products.json").then(res => res.json()).then(data=>{
+                console.log(data);
+            });
+        } catch (error) {
+            console.log("Error in Javascript");
+        }
+    }
+    
+    return (
+        <>
+            <div className="maine">
+                <center>
+                    <div className='cardse'>
+                        {JsonData.map((item, index) => (
+                            <Card id={item.id} productName={item.productName} description={item.description} productPrices={item.prices}></Card>
+                        ))}
+                    </div>
+                </center>
+            </div>
+        </>
     )
 }
