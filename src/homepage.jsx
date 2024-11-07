8ea7f2b74927d319b715646d29a3f9f84ee246f7
akshatverma1 <akshaverma14@gmail.com>
2024-11-07 13:03:08 +0530

    Backgound

---
+++ src/homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./homepage.css"
+export default function Homepagefunction() {
+    return (
+                <div className="maine">
+                    
+                </div>
+    )
+}
