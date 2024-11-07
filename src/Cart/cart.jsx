954e24ffc29bd53b40bdc7d74dade1f6982a021c
akshatverma1 <akshaverma14@gmail.com>
2024-11-08 03:24:04 +0530

    More Details

--- src/Cart/cart.jsx
+++ src/Cart/cart.jsx
@@ -4,9 +4,14 @@ import { useState } from 'react';
 export default function CartFunction(){
     const location = useLocation();
     let cart = location.state.cartDetails;
+    let[deleteIN,setDeleteIN]= useState(cart);
     let[del,setDel] = useState(cart);
     const delectBtn =()=>{
         setDel([]);
+    }
+    const DelectIndivial=(itemdel)=>{
+        const newCart = del.filter((item)=>item.id!==itemdel.id);
+        setDel(newCart);
     }
     return (
         <>
@@ -14,8 +19,8 @@ export default function CartFunction(){
             {del.map((item,index)=>{
                 return(
                     <div>
-                    <h1 style={{color:"white"}}>{item.productName}</h1>
-                    <button>Delect Button</button>
+                    <h1 style={{color:"white"}}>{item.productName}</h1> 
+                    <button onClick={()=> DelectIndivial(item)}>Delect Button</button>
                     </div>
                 )
             })}
