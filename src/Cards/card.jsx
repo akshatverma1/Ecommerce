8aeb72c2d41d32c4a48f3c289c004ddb7102e6be
akshatverma1 <akshaverma14@gmail.com>
2024-11-07 14:54:27 +0530

    Cards

--- src/Cards/card.jsx
+++ src/Cards/card.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import "./card.css"
+import Image from "./airconn.svg";
 export default function CardFunction() {
     return (
         <>
             <div class="card">
                 <div class="card-details">
+                    <img src={Image} alt="Sample Image" width="50px" height="50px"/>
                     <p class="text-title">Card title</p>
                     <p class="text-body">Here are the details of the card</p>
                 </div>
-                <button class="card-button">More info</button>
+                <button class="card-button">Add To Cart</button>
             </div>
         </>
     )
--- src/homepage.jsx
+++ src/homepage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import "./homepage.css"
 import Card from "./Cards/card.jsx";
+import Navbar from './Navbar/navbar.jsx';
 export default function Homepagefunction() {
     return (
                 <div className="maine">
+                    <Navbar></Navbar>
                     <center>
                     <div className='cardse'>
                         <Card></Card>
