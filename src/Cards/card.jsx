5325bcbc3eab32f5f8fb5ae0d9098695cb303d8d
akshatverma1 <akshaverma14@gmail.com>
2024-11-07 13:32:29 +0530

    Cards

---
+++ src/Cards/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./card.css"
+export default function CardFunction() {
+    return (
+        <>
+            <div class="card">
+                <div class="card-details">
+                    <p class="text-title">Card title</p>
+                    <p class="text-body">Here are the details of the card</p>
+                </div>
+                <button class="card-button">More info</button>
+            </div>
+        </>
+    )
+}
--- src/homepage.jsx
+++ src/homepage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import "./homepage.css"
+import Card from "./Cards/card.jsx";
 export default function Homepagefunction() {
     return (
                 <div className="maine">
-                    
+                    <center>
+                    <div className='cardse'>
+                        <Card></Card>
+                        <Card></Card>
+                        <Card></Card>
+                        <Card></Card>
+                        <Card></Card>
+                        <Card></Card>
+                    </div>
+                    </center>
                 </div>
     )
 }
