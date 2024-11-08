67d09462e4db1428440dcc6fed713d55dbd1200e
akshatverma1 <akshaverma14@gmail.com>
2024-11-08 21:10:38 +0530

    Complate

---
+++ src/Animation/heading.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./heading.css"
+export default function Headingfunction() {
+    return (
+        <>
+            <div class="cardll">
+                <div class="loaderll">
+                <p >E-Commerce </p>
+                    <div class="wordsll">
+                        <span style ={{color:"#6afab0",marginLeft:"1rem"}}class="wordlll"> EaseWork AI</span>
+                        <span class="wordlll">EaseWork AI</span>
+                        <span style ={{color:"#6afab0"}}class="wordlll">Develop By Akshat</span>
+                        <span class="wordlll">EaseWork AI</span>
+                        <span style ={{color:"#6afab0"}}class="wordlll">Develop By Akshat</span>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
--- src/homepage.jsx
+++ src/homepage.jsx
@@ -3,41 +3,51 @@ import "./homepage.css"
 import Card from "./Cards/card.jsx";
 import Navbar from './Navbar/navbar.jsx';
 import JsonData from "./ItemData/electronics_products.json";
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 import "./Navbar/navbar.css"
 import { Link } from 'react-router-dom';
+import Headingfunction from './Animation/heading.jsx';
 export default function Homepagefunction() {
     const navigator = useNavigate();
-    const cartButton=()=>{
-        navigator("/cart",{state :{cartDetails : cart}});
+    const cartButton = () => {
+        navigator("/cart", { state: { cartDetails: cart } });
     }
 
-    async function call(){
+    async function call() {
         try {
-            await fetch("./ItemData/electronics_products.json").then(res => res.json()).then(data=>{
+            await fetch("./ItemData/electronics_products.json").then(res => res.json()).then(data => {
                 console.log(data);
             });
         } catch (error) {
             console.log("Error in Javascript");
         }
     }
-    
-    const [cart,setCart]= useState([]);
-    const [count,setCount]= useState(0);
-    const addToCart=(data)=>{
-        setCart([...cart,data]);
-        setCount(count+1);
+
+    const [cart, setCart] = useState([]);
+    const [count, setCount] = useState(0);
+    const addToCart = (data) => {
+        setCart([...cart, data]);
+        setCount(count + 1);
     }
     return (
         <>
-        <nav className="navbar">
-      <div className="navbar-brand">Brand</div>
-      <ul className="navbar-links">
-        <Link to={"/"}><li>Home</li></Link>         
-        <button>Cart</button>
-      </ul>
-    </nav>
-        <button onClick={cartButton} style={{backgroundColor:"white", color:"black"}}>{count} Cart</button>
+            <nav className="navbar">
+                <Headingfunction></Headingfunction>
+                <ul className="navbar-links">
+                    <Link className="linkhome" to={"/"}><li><i class="fa-solid fa-house"></i>    Home</li></Link>
+                    <button onClick={cartButton} className="cartbtn">
+                        <strong><i class="fa-solid fa-cart-shopping"> {count}</i></strong>
+                        <div id="container-stars">
+                            <div id="stars"></div>
+                        </div>
+
+                        <div id="glow">
+                            <div class="circle"></div>
+                            <div class="circle"></div>
+                        </div>
+                    </button>
+                </ul>
+            </nav>
             <div className="maine">
                 <center>
                     <div className='cardse'>
