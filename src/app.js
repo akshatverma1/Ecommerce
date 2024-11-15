52ae6ce842168460539fd748b56c27c60c41a056
akshatverma1 <akshaverma14@gmail.com>
2024-11-15 23:33:45 +0530

    Message

--- src/app.js
+++ src/app.js
@@ -3,11 +3,6 @@ import React from 'react';
 export default function AppFunction(){
     return (
         <>
-        <h1>Akshat</h1>
-        <h1>Akshat</h1>
-        <h1>Akshat</h1>
-        <h1>Akshat verma
-        </h1>
         </>
     )
 }
